8d7bdfbf5d4e251825abd99ad4919d548e38f6c7
Blessings Kalambai <bkalambai@gmail.com>
2021-03-07 12:39:45 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,5 @@
 export const environment = {
   production: true,
   config: {
-    apiKey: "AIzaSyDABTz4QJ6f20o-H1bUHbdqHdQXDYQXprY",
-    authDomain: "loansystem-2cfbd.firebaseapp.com",
-    projectId: "loansystem-2cfbd",
-    storageBucket: "loansystem-2cfbd.appspot.com",
-    messagingSenderId: "1076888833167",
-    appId: "1:1076888833167:web:2f2652ff0751c8bac874fa",
-    measurementId: "G-7LPBLG67ZW"
   }
 };
